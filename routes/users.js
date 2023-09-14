4d68551bcd5c28b4544507c1845aeb311320713d
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-14 08:30:51 +0700

    feat: create get router and post router

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+const users = [];
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.post('/', (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+export default router;
